133545bbacb794f7712b61c3177e1a7843fbf9f6
acs-web-tech <aruncsedeptjacsi@gmail.com>
2024-09-01 01:32:53 +0530

    ui-home

---
+++ tmr/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar  from './Components/Navbar';
+import MainContent from './Components/Main';
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <MainContent></MainContent>
+    </>
+  );
+}
+
+export default App;
